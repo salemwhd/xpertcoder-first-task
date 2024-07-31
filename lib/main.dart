ce24d6fc7ce9932e69845e90cd42e2449fe72597
salem wahed <salemwhd@gmail.com>
2024-07-31 21:43:22 +0300

    add github images

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:first_task/views/booking_page.dart';
 import 'package:first_task/views/home_page.dart';
 import 'package:first_task/views/room_page.dart';
+import 'package:first_task/views/rooms_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomePage(),
+      home: BookingPage(imagePath: 'assets/images/room_image1.png'),
     );
   }
 }
